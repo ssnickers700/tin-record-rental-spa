212f18a007650af37edeb5b290da8ce08503ee2a
Patryk <ssnickers700@gmail.com>
2022-12-24 13:10:55 +0100

    Add footer component and install react router package

--- record-rental/src/App.js
+++ record-rental/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/fragments/Header";
 import Navigation from "./components/fragments/Navigation";
 import MainContent from "./components/other/MainContent";
+import Footer from "./components/fragments/Footer";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header/>
             <Navigation/>
             <MainContent/>
+            <Footer />
         </div>
     );
 }
---
+++ record-rental/src/components/fragments/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            Patryk Leszczyński s20747
+        </footer>
+    )
+}
+
+export default Footer
