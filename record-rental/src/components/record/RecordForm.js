084adb97296f76cb38283b465034d845c6efe41f
Patryk <ssnickers700@gmail.com>
2022-12-27 15:24:39 +0100

    Change form components into functional

--- record-rental/src/components/record/RecordForm.js
+++ record-rental/src/components/record/RecordForm.js
@@ -1,37 +1,36 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-class RecordForm extends React.Component{
-    render() {
-        return (
-            <main>
-                <h2>Nowa płyta</h2>
-                <form className="form">
-                    <label htmlFor="recordName">Tytuł: <span className="symbol-required">*</span></label>
-                    <input type="text" name="recordName" id="recordName" className="error-input" required/>
-                    <span id="errorRecordName" className="errors-text"></span>
+function RecordForm() {
+    return (
+        <main>
+            <h2>Nowa płyta</h2>
+            <form className="form">
+                <label htmlFor="recordName">Tytuł: <span className="symbol-required">*</span></label>
+                <input type="text" name="recordName" id="recordName" className="error-input" required/>
+                <span id="errorRecordName" className="errors-text"></span>
 
-                    <label htmlFor="artistName">Artysta: <span className="symbol-required">*</span></label>
-                    <input type="text" name="artistName" id="artistName" className="error-input" required/>
-                    <span id="errorArtistName" className="errors-text"></span>
+                <label htmlFor="artistName">Artysta: <span className="symbol-required">*</span></label>
+                <input type="text" name="artistName" id="artistName" className="error-input" required/>
+                <span id="errorArtistName" className="errors-text"></span>
 
-                    <label htmlFor="price">Cena za dzień: <span className="symbol-required">*</span></label>
-                    <input type="number" min="0.00" name="price" id="price" className="error-input" required/>
-                    <span id="errorPrice" className="errors-text"></span>
+                <label htmlFor="price">Cena za dzień: <span className="symbol-required">*</span></label>
+                <input type="number" min="0.00" name="price" id="price" className="error-input" required/>
+                <span id="errorPrice" className="errors-text"></span>
 
-                    <label htmlFor="unit">Ilość dostępnych: <span className="symbol-required">*</span></label>
-                    <input type="number" name="unit" id="unit" className="error-input" required/>
-                    <span id="errorUnit" className="errors-text"></span>
+                <label htmlFor="unit">Ilość dostępnych: <span className="symbol-required">*</span></label>
+                <input type="number" name="unit" id="unit" className="error-input" required/>
+                <span id="errorUnit" className="errors-text"></span>
 
-                    <div className="form-buttons">
-                        <p id="errorsSummary" className="errors-text"></p>
-                        <input type="submit" value="Dodaj" className="form-button-submit-add"/>
-                        <Link to="/records" className="form-button-cancel">Anuluj</Link>
-                    </div>
-                </form>
-            </main>
-        );
-    }
+                <div className="form-buttons">
+                    <p id="errorsSummary" className="errors-text"></p>
+                    <input type="submit" value="Dodaj" className="form-button-submit-add"/>
+                    <Link to="/records" className="form-button-cancel">Anuluj</Link>
+                </div>
+            </form>
+        </main>
+    );
 }
+
 
 export default RecordForm
--- record-rental/src/components/rental/RentalForm.js
+++ record-rental/src/components/rental/RentalForm.js
@@ -3,52 +3,51 @@ import {Link} from "react-router-dom";
 import {getClientsApiCall} from "../../apiCalls/clientApiCalls";
 import {getRecordsApiCall} from "../../apiCalls/recordApiCalls";
 
-class RentalForm extends React.Component{
-    render() {
-        const allClients = getClientsApiCall();
-        const allRecords = getRecordsApiCall();
+function RentalForm() {
+    const allClients = getClientsApiCall();
+    const allRecords = getRecordsApiCall();
 
-        return (
-            <main>
-                <h2>Nowy wynajem</h2>
-                <form className="form">
-                    <label htmlFor="client">Klient: <span className="symbol-required">*</span></label>
-                    <select name="client" id="client" required>
-                        <option value>Wybierz klienta</option>
-                        {allClients.map(client =>
-                            (<option key={client._id} value={client._id}
-                                     label={client.firstName + " " + client.lastName}>
-                            </option>)
-                        )}
-                    </select>
-                    <span id="errorClient" className="errors-text"></span>
+    return (
+        <main>
+            <h2>Nowy wynajem</h2>
+            <form className="form">
+                <label htmlFor="client">Klient: <span className="symbol-required">*</span></label>
+                <select name="client" id="client" required>
+                    <option value>Wybierz klienta</option>
+                    {allClients.map(client =>
+                        (<option key={client._id} value={client._id}
+                                 label={client.firstName + " " + client.lastName}>
+                        </option>)
+                    )}
+                </select>
+                <span id="errorClient" className="errors-text"></span>
 
-                    <label htmlFor="record">Płyta: <span className="symbol-required">*</span></label>
-                    <select name="record" id="record" required>
-                        <option value>Wybierz płytę</option>
-                        {allRecords.map(record =>
-                            (<option key={record._id} value={record._id}
-                                     label={record.recordName + " - " + record.artistName}>
-                            </option>)
-                        )}
-                    </select>
-                    <span id="errorRecord" className="errors-text"></span>
+                <label htmlFor="record">Płyta: <span className="symbol-required">*</span></label>
+                <select name="record" id="record" required>
+                    <option value>Wybierz płytę</option>
+                    {allRecords.map(record =>
+                        (<option key={record._id} value={record._id}
+                                 label={record.recordName + " - " + record.artistName}>
+                        </option>)
+                    )}
+                </select>
+                <span id="errorRecord" className="errors-text"></span>
 
-                    <label htmlFor="startDate">Data od:</label>
-                    <input type="date" name="startDate" id="startDate" required />
+                <label htmlFor="startDate">Data od:</label>
+                <input type="date" name="startDate" id="startDate" required/>
 
-                    <label htmlFor="endDate">Data do:</label>
-                    <input type="date" name="endDate" id="endDate" />
+                <label htmlFor="endDate">Data do:</label>
+                <input type="date" name="endDate" id="endDate"/>
 
-                    <div className="form-buttons">
-                        <p id="errorsSummary" className="errors-text"></p>
-                        <input type="submit" value="Dodaj" className="form-button-submit-add"/>
-                        <Link to="/rentals" className="form-button-cancel">Anuluj</Link>
-                    </div>
-                </form>
-            </main>
-        );
-    }
+                <div className="form-buttons">
+                    <p id="errorsSummary" className="errors-text"></p>
+                    <input type="submit" value="Dodaj" className="form-button-submit-add"/>
+                    <Link to="/rentals" className="form-button-cancel">Anuluj</Link>
+                </div>
+            </form>
+        </main>
+    );
 }
+
 
 export default RentalForm
