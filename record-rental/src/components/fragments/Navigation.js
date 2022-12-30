4225cf16b251805fa62cba6102a4e2fb81fb2df9
Patryk <ssnickers700@gmail.com>
2022-12-30 21:07:58 +0100

    Make navlink active style react to tab change

--- record-rental/src/components/fragments/Navigation.js
+++ record-rental/src/components/fragments/Navigation.js
@@ -1,14 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 
 function Navigation() {
     return (
         <nav>
             <ul>
-                <li><Link to="/" className="active">Strona główna</Link></li>
-                <li><Link to="/clients">Klienci</Link></li>
-                <li><Link to="/records">Płyty</Link></li>
-                <li><Link to="/rentals">Wynajmy</Link></li>
+                <li><NavLink to="/" activeClassName="active">Strona główna</NavLink></li>
+                <li><NavLink to="/clients" activeClassName="active">Klienci</NavLink></li>
+                <li><NavLink to="/records" activeClassName="active">Płyty</NavLink></li>
+                <li><NavLink to="/rentals" activeClassName="active">Wynajmy</NavLink></li>
             </ul>
         </nav>
     )
